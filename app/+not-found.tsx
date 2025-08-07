25e612c708d15c0978ccaf08ac89cc54647402ec
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2025-08-07 18:53:54 -0400

    feat: add screen for not found website path

---
+++ app/+not-found.tsx
@@ -0,0 +1,27 @@
+import { Link } from "expo-router";
+import { StyleSheet, View } from "react-native";
+import { ThemedText } from "@/components/ThemedText";
+
+export default function NotFoundScreen() {
+    return (
+        <View style={styles.container}>
+            <ThemedText type="title">This screen does not exist.</ThemedText>
+            <Link href="/" style={styles.link}>
+                <ThemedText type="link">Return to Blackjack!</ThemedText>
+            </Link>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 20,
+    },
+    link: {
+        marginTop: 15,
+        paddingVertical: 15,
+    },
+});
