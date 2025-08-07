826567c56727fe137522c833da92b2c7eb12908d
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2025-08-07 18:49:05 -0400

    add: colors

---
+++ constants/Colors.ts
@@ -0,0 +1,42 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+const shadow = "#000000";
+
+export const Colors = {
+    background: "#151718",
+    card: {
+        hearts: "red",
+        diamonds: "red",
+        spades: "black",
+        clubs: "black",
+        face: "white",
+        back: "darkred",
+        border: "black",
+    },
+    overlay: "rgba(0, 0, 0, 0.5)",
+    table: {
+        felt: "#247345",
+        border: "#340a0d",
+        shadow: shadow,
+    },
+    ui: {
+        background: "#c9bfab",
+        translucentBackground: "rgba(201, 191, 171, 0.8)",
+        border: "#6d442b",
+        text: {
+            plain: "#11181C",
+            contrast: "#fffbf3",
+            theme: "#6d442b",
+        },
+        button: {
+            background: "#fffbf3",
+            foreground: "#6d442b",
+        },
+        shadow: shadow,
+        tint: "#0a7ea4",
+        link: "#0a7ea4",
+    },
+};
