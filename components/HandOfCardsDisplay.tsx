3ddbaa99f459c506654f631bb5af6c06494e8d87
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2025-08-17 04:37:42 -0400

    feat(HandOfCardsDisplay): add basic display of cards component

---
+++ components/HandOfCardsDisplay.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { HandOfCards } from "./HandOfCards";
+import PlayingCard from "./PlayingCard";
+import { VisibilityHandOfCards } from "./VisibilityHandOfCards";
+import { StyleProp, StyleSheet, View, ViewStyle } from "react-native";
+
+export enum HandOfCardsVariants {
+    PLAYER = "Player",
+    DEALER = "Dealer",
+}
+
+interface HandOfCardsProps {
+    variant: HandOfCardsVariants;
+    hand: HandOfCards | VisibilityHandOfCards;
+    style?: StyleProp<ViewStyle>;
+}
+
+const cardOverlap = -75;
+const centerOffset = cardOverlap / 4;
+
+export default function HandOfCardsDisplay({ variant, hand, style }: HandOfCardsProps) {
+    if (hand instanceof VisibilityHandOfCards) {
+        let hiddenCards = hand.getHiddenCards();
+        let cards = [...hiddenCards, ...hand.getVisibleCards()];
+        return (
+            <View style={[styles.container, style]}>
+                {cards.map((card, idx) => (
+                    <PlayingCard
+                        card={card}
+                        flipped={idx < hiddenCards.length}
+                        style={{ left: cardOverlap * idx - centerOffset * cards.length, zIndex: idx }}
+                        key={variant + "-hand-card-" + idx}
+                    />
+                ))}
+            </View>
+        );
+    }
+    let cards = hand.getHand();
+    return (
+        <View style={[styles.container, style]}>
+            {cards.map((card, idx) => (
+                <PlayingCard card={card} style={{ left: cardOverlap * idx - centerOffset * cards.length, zIndex: idx }} key={variant + "-hand-card-" + idx} />
+            ))}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        position: "relative",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
